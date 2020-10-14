5ab37a073123d79ade6f69222259bfd7d8288fee
Opilane TTHK <opilane@tthk.ee>
2020-10-14 16:11:12 +0300

    .

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            int Celsius, Fahrenheit, UserChoice;
+
+
+            Console.SetCursorPosition(15, 0);
+            Console.WriteLine("Welcome to the Temperature Converter!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+
+            Console.SetCursorPosition(0, 9);
+            Console.WriteLine("Please Enter one of the provided options from above");
+            UserChoice = Convert.ToInt16(Console.ReadLine());
+
+            Console.Clear();
+
+
+            // Convert Celsius to Fahrenheit.
+            if (UserChoice == 1)
+            {
+                Console.SetCursorPosition(20, 0);
+                Console.WriteLine("Converting Celsius To Fahrenheit");
+
+                Console.SetCursorPosition(0, 4);
+                Console.WriteLine("Enter the Temperature in Celsius(°C) : ");
+                Celsius = int.Parse(Console.ReadLine());
+                Fahrenheit = (Celsius * 9) / 5 + 32;
+                Console.WriteLine("The temperature in Fahrenheit is(°F) : " + Fahrenheit);
+
+
+                if (Fahrenheit <= -50)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                if (Fahrenheit <= -10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                if (Fahrenheit == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                if (Fahrenheit >= 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                if (Fahrenheit >= 50)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                    Console.ReadLine();
+
+
+
+                }
+
+
+                // Convert Fahrenheit to Celsius.
+
+                if (UserChoice == 2)
+                {
+                    Console.SetCursorPosition(20, 0);
+                    Console.WriteLine("Converting Fahrenheit To Celsius");
+
+                    Console.SetCursorPosition(0, 4);
+                    Console.WriteLine("Enter the Temperature in Fahrenheit(°F) : ");
+                    Fahrenheit = int.Parse(Console.ReadLine());
+                    Celsius = (Fahrenheit - 32) * 5 / 9;
+                    Console.WriteLine("The temperature in Celsius is(°C) : " + Celsius);
+
+                    if (Celsius <= -50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    if (Celsius <= -10)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    if (Celsius == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    if (Celsius >= 10)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    if (Celsius >= 50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+                        Console.ReadLine();
+
+
+                        //Exit
+
+                        if (UserChoice != 3)
+                        {
+
+                            Console.ReadLine();
+
+                            //Help Facility
+
+                            if (UserChoice == 4)
+                            {
+                                Console.SetCursorPosition(20, 0);
+                                Console.WriteLine("Welcome to the Temperature Calculator Help Facility");
+                                Console.WriteLine("");
+
+                                Console.ReadLine();
+                            }
+
+
+                            {
+
+
+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Week7Converters/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Week7Converters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program in C# Sharp that converts
+             * Fahrenheit into Celcius and vice versa.
+             * -The program enables the user to select
+             * if the user wants to convert into celcius 
+             * or into Farenheit
+             * - After the user has made their choice,
+             * the program asks for an input,
+             * performs calculations and
+             * displays the result
+             * - fahrenheit = (celsius * 9) / 5+32;
+             * - celsius = (fahrenheit - 32) * 5 / 9;
+             */
+            Console.WriteLine("Do you wish to convert a number into Fahrenheit or Celcius or vice versa?");
+            string UserInput = Console.ReadLine().ToLower();
+            int Fahrenheit, Celcius;
+            
+
+            if (UserInput == "fahrenheit")
+            {
+                Console.WriteLine("Enter the temperature in Celsius (°C): ");
+                Celcius = int.Parse(Console.ReadLine());
+                Fahrenheit = (Celcius * 9) / 5 + 32;
+                Console.WriteLine("The temperature in Fahrenheit is(°F) : " + Fahrenheit);
+            } else if (UserInput == "celcius")
+            {
+                Console.WriteLine("Enter the temperature in Fahrenheit (°F): ");
+                Fahrenheit = int.Parse(Console.ReadLine());
+                Celcius = (Fahrenheit - 32) * 5 / 9;
+                Console.WriteLine("The temperature in Celsius is(°C) : " + Celcius);
+            }else
+            {
+                Console.WriteLine("Soooo, you feel like writing what I actually asked of you?");
+            }
+
+        }
+
+
+
+    }
+}
